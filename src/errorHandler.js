0e249e469de7876c1fa6540fe020f1cc3a6b4763
umutplvn <umutpehlivan2078@gmail.com>
2023-10-06 01:43:24 +0100

    up

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,35 +1,17 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// Catch Errors:
 
-//Catch Errors;
+module.exports = (err, req, res, next) => {
 
-module.exports = (err, req, res, next)=>{
-   
-    const errorStatusCode=res.errorStatusCode ?? 500
+    const errorStatusCode = res?.errorStatusCode ?? 500
+
     res.status(errorStatusCode).send({
-        error:true,
-        message:err.message,    //err parametresi
-        cause:err.cause,     //err parametresi
-        body: req.body  // sending json data
+        error: true,
+        message: err.message,
+        cause: err.cause,
+        body: req.body
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
